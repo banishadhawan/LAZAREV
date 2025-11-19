e4901a9f098531746bb8a66ddca2eb93f0e13f9c
banisha <banisha@gmail.com>
2025-11-19 19:59:57 +0530

    Completed and updated all

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+function locomotiveAnimation() {  //locomotive scroll provides smooth scrolling, scroll-based reveal/animations
+     gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
+
+
 function navAnimation(){
     var nav = document.querySelector("nav")
 
@@ -5,17 +42,19 @@ nav.addEventListener("mouseenter",function(){
     let tl = gsap.timeline()  //A timeline lets you sequence multiple animations in order, so they play one after another smoothly
 
     tl.to("#nav-bottom",{  //.to() means GSAP will animate from its current height → to 21vh
-        height: "21vh"
+        height: "21vh",
+        duration: 0.5
     })
     tl.to(".nav-part2 h5",{
-        display: "block"  //Hidden <h5> elements appear
+        display: "block",  //Hidden <h5> elements appear
+        duration: 0.1
     })
     tl.to(".nav-part2 h5 span",{
-        y:25,  //.from() means the elements start 20px below their final position (y:20) and animate upward to their original position
+        y:0,  //.from() means the elements start 20px below their final position (y:20) and animate upward to their original position
         //stagger in GSAP is used to delay the start of each element’s animation when you’re
         //animating multiple elements together - not everything moves at once
         stagger:{  //makes each span animate one after another with a 0.1 second delay — creating a smooth "wave" or "staggered" entrance effect
-            amount: 0.6  //amount means the total duration of the staggering effect across all elements.
+            amount: 0.5  //amount means the total duration of the staggering effect across all elements.
                         //The delay between each element is automatically calculated based on how many elements you’re animating.
                     //GSAP will distribute that 0.6 seconds evenly across all 6 items So each element starts roughly 0.1 seconds apart
         }  
@@ -40,7 +79,7 @@ nav.addEventListener("mouseleave",function(){
     })
 })
 }
-navAnimation();
+
 
 function page2Animation(){
     
@@ -67,32 +106,29 @@ rightElems.forEach(function(elem){
     })
 })
 }
-page2Animation();
+
 
 function page3VideoAnimation(){
     var page3Center = document.querySelector(".page3-center")
-var video = document.querySelector("#page3 video")
+    var video = document.querySelector("#page3 video")
 
-page3Center.addEventListener("click",function(){
-    video.play();  //This starts the video playback
-    gsap.to(video,{
-        transform: "scaleX(1) scaleY(1)",  //set width back to 100% ,set height back to 100%
-        opacity: 1,
-        borderRadius: 0
+    page3Center.addEventListener("click",function(){
+        video.play();  //This starts the video playback
+        gsap.to(video,{
+            transform: "scaleX(1) scaleY(1)",  //set width back to 100% ,set height back to 100%
+            opacity: 1,
+            borderRadius: 0
+        })
+
     })
-
-})
-video.addEventListener("click",function(){
-    video.pause()
-    gsap.to(video,{
-        transform: "scaleX(0.7) scaleY(0)",  
-        opacity: 0,
-        borderRadius: "30px"
+    video.addEventListener("click",function(){
+        video.pause()
+        gsap.to(video,{
+            transform: "scaleX(0.7) scaleY(0)",  
+            opacity: 0,
+            borderRadius: "30px"
+        })
     })
-})
-}
-page3VideoAnimation()
-
 
     var sections = document.querySelectorAll(".sec-right")
 
@@ -106,3 +142,58 @@ page3VideoAnimation()
             elem.childNodes[3].load()  //load = restart video from the beginning
         })
     })
+}
+
+
+function page6Animations() {
+    gsap.from("#btm6-part2 h4", {
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#btm6-part2",
+            scroller: "#main",
+            // markers:true,
+            start: "top 80%",
+            end: "top 10%",
+            scrub: true
+        }
+    })
+}
+
+
+function loadingAnimation() {
+
+    var tl = gsap.timeline()
+    tl.from("#page1", {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.2
+    })
+    tl.from("#page1", {
+        transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+        borderRadius: "150px",
+        duration: 2,
+        ease: "expo.out"
+    })
+    tl.from("nav", {
+        opacity: 0,
+        delay: -0.2
+    })
+    tl.from("#page1 h1, #page1 p, #page1 div", {
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.2
+    })
+}
+
+locomotiveAnimation()
+
+navAnimation()
+
+page2Animation()
+
+page3VideoAnimation()
+
+page6Animations()
+
+loadingAnimation()
